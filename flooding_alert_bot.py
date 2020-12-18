aa552299cc11e5733258bafdf51873f62605eb08
loydp <54460504+loydp@users.noreply.github.com>
2020-12-18 02:39:37 -0800

    flooding file eviscerated a bit, to make building up easier

--- flooding_alert_bot.py
+++ flooding_alert_bot.py
@@ -1,29 +1,32 @@
 import pandas as pd
-url = "https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=CRNW1&output=tabular"
-url2 = "https://readingsvc.azurewebsites.net/api/GetGageReadingsUTC?regionId=1&id=SVPA-17&fromDateTime=2020-01-06T16:00:07-08:00&toDateTime=2020-01-10T16:00:07-08:00&showDeletedReadings=false"
-'''
-SQUW1
-CRNW1
-CRNZ1
-'''
+SVPA_test_url = "https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=CRNW1&output=tabular"
+USGS_test_url = "https://readingsvc.azurewebsites.net/api/GetGageReadingsUTC?regionId=1&id=SVPA-17&fromDateTime=2020-01-06T16:00:07-08:00&toDateTime=2020-01-10T16:00:07-08:00&showDeletedReadings=false"
 
-"""
-Every few minutes a class activates a retrieval routine.
-At a time based on past data it retrieves information from the gages.
-It sends a message to each gage and then stores that information in a class
-associated with that gage, deleting old data.
-Then:
-It looks through the predicted flood level data, and if it exceeds a certain level
-(stored in class) and unless told otherwise will send an alert.
-The always on class also responds to requests from the bot source.
+class Gage:
 
-"""
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+        print(name, 'gage is now initialized')
 
-class DataRetriever:
+
+class FloodAlertBot:
 
     def __init__(self):
         print('GetUSGS initialized')
 
+
+if __name__ == '__main__':
+    data_get = FloodAlertBot()
+
+    # svpa = data_get.get_SVPA_reading(USGS_test_url)
+    # usgs = data_get.get_USGS_reading(SVPA_test_url)
+    
+    # print(usgs[1])
+    # print(svpa['readings'][2])
+    
+
+'''
     def get_USGS_reading(self, url):
         try:
             df_list = pd.read_html(url)
@@ -39,15 +42,4 @@ class DataRetriever:
         except Exception as e:
             print("Error in GetSVPA, get_SVPA:\n", e)
             return None
-
-
-
-
-if __name__ == '__main__':
-    data_get = DataRetriever()
-    svpa = data_get.get_SVPA_reading(url2)
-    usgs = data_get.get_USGS_reading(url)
-    
-    print(usgs[1])
-    print(svpa['readings'][2])
-    
+'''
